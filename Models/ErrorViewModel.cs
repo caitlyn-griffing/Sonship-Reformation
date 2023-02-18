6dc196edbf348130561aa4a4b92002e028997e5b
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2023-02-17 18:31:02 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SonshipReformation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
